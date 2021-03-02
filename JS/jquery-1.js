5d9138537e65e11597c6a5279bcdde5a384c83a3
KbBugti <khudabakhsh.bugti@powercoders.org>
2021-03-02 01:01:28 +0100

    first commit

--- JS/jquery-1.js
+++ JS/jquery-1.js
@@ -1,6 +1,9 @@
-// Modifying elements with jQuery
+
+var crocodilize = function(){
+  // Modifying elements with jQuery
 $("h2").text("CROCODILE ATTACKED!");
 $("p").html(" The crocodiles are coming - <strong>Oh noess</strong>!");
+ 
 
 // for loop
 var $paragraphs = $('p');
@@ -14,7 +17,7 @@ for (var i = 0; i < $paragraphs.length; i++) {
 $paragraphs.each(function(index, element) {
     var $paragraph = $(this);
     console.log(element === this);
-    $paragraph.html( $paragraph.html() + "..wowee!");
+    $paragraph.html( $paragraph.html() + "..Wow amazing!");
 });
 
 // Add CSS roules
@@ -52,3 +55,96 @@ $heading.html("Using jQuery again!");
 var headingD = $heading[0];
 
 var $headingJ = $(headingD);
+
+
+// Daisy Chain - Making Webpages Interactive with jQuery
+for (var i = 0; i < 6; i++ ){
+    $("<img>")
+    .attr("src","https://upload.wikimedia.org/wikipedia/commons/2/29/English_Daisy_(Bellis_Perennis).jpg")
+     .attr("width","50")
+     .attr("alt","A white and yallow daisy")
+     .appendTo("body")
+}
+
+}
+
+$("#croc-button").on("click", function(){
+    console.log("Click here!");
+    crocodilize();
+});
+
+// When the user clicks on the page:
+//   create a new image
+//   position it according to where click happened
+
+
+// $("body").on("click", function(event){
+//     console.log(event);
+//     var $img = $("<img>")
+//     .attr("src", "https://www.kasandbox.org/programming-images/animals/crocodiles.png")
+//     .width("100")
+//     .css("position", "absolute")
+//     .css("top", event.pageY + "px")
+//     .css("left", event.pageX + "px")
+//     .appendTo("body");
+// });
+
+
+// $("#pic").on("click", function(event) {
+//     var mouseX = event.pageX;
+//     var mouseY = event.pageY;
+//  });
+
+
+    // When the user submits the form,
+    // Check what answer they picked
+    // And tell them if they're correct
+
+    // submit
+    $("#quiz-form").on("submit", function(event){
+        event.preventDefault();
+        var $answer = $("#quiz-answer");
+        var answer = $answer.val();
+        console.log(answer);
+        if (answer === "crocodile"){
+            $("#result").text("Woweeee! You got it! WOOO PARTY!");
+        } else {
+            $("#result").text("Try again!");
+        }
+    });
+
+// Maps croc species to image URLs
+var crocImages = {
+    "american": "https://upload.wikimedia.org/wikipedia/commons/f/f2/Crocodylus_acutus_mexico_02-edit1.jpg",
+    "saltwater": "https://upload.wikimedia.org/wikipedia/commons/c/ca/Crocodylus_porosus_4.jpg",
+    "freshwater": "https://upload.wikimedia.org/wikipedia/commons/c/ce/Australia_Cairns_18.jpg"
+};
+
+// When the user submits the form,
+// Check what species they ordered
+// and display an image of it
+
+$("#croc-form").on("submit", function(event){
+    event.preventDefault();
+        var $crocSpecies = $(this).find("[name=species]"); // scoped
+    var crocSpecies = $crocSpecies.val();
+    var $img = $("<img>");
+    $img.width(100);
+    $img.attr("src", crocImages[crocSpecies]);
+    $img.appendTo("body");
+});
+
+
+var calcDonuts = function(years) {
+    return years * 3 * 365;
+};
+
+$("#calculator-form").on("submit", function(event) {
+    event.preventDefault();
+    var $age = $(this).find("[name=age]");
+    var age = $age.val();
+    var donuts = calcDonuts(age);
+
+    $("#calculator-results").text("Winston ate " + donuts + " donuts");
+    
+});
--- JS/jquery.js
+++ JS/jquery.js
@@ -1,43 +1,40 @@
 
+DOM 
+window.onload = function(){
+    const heading = document.createElement("h2");
+    const heading_text = document.createTextNode("Big Head");
+    heading.appendChild(heading_text);
+    document.body.appendChild(heading);
+}
+
+window.onload = function(){
+    const h2_tag = document.createElement("h3");
+    const insert_text = document.createTextNode("H3 Heading");
+    h2_tag.appendChild(insert_text);
+    document.body.appendChild(h2_tag);
+}
 
 
-
-// DOM 
-// window.onload = function(){
-//     const heading = document.createElement("h2");
-//     const heading_text = document.createTextNode("Big Head");
-//     heading.appendChild(heading_text);
-//     document.body.appendChild(heading);
-// }
-
-// window.onload = function(){
-//     const h2_tag = document.createElement("h3");
-//     const insert_text = document.createTextNode("H3 Heading");
-//     h2_tag.appendChild(insert_text);
-//     document.body.appendChild(h2_tag);
-// }
-
-
-//value from 0 to 1
-// function getColor(value){
-//     var hue=((1-value)*120).toString(2);
-//     return ["hsl(",hue,",100%,50%)"].join("");
-// }
-// var len=20;
-// for(var i=0; i<=len; i++){
-//     var value=i/len;
-//     var d=document.createElement('div');
-//     d.textContent="value="+value;
-//     d.style.backgroundColor=getColor(value);
-//     document.body.appendChild(d);
-// }
+// value from 0 to 1
+function getColor(value){
+    var hue=((1-value)*120).toString(2);
+    return ["hsl(",hue,",100%,50%)"].join("");
+}
+var len=20;
+for(var i=0; i<=len; i++){
+    var value=i/len;
+    var d=document.createElement('div');
+    d.textContent="value="+value;
+    d.style.backgroundColor=getColor(value);
+    document.body.appendChild(d);
+}
 
 
 // jQuery
 
-// $("h1"); // selects all the h1s
-// $("#heading"); // selects the element with id of "heading"
-// $(".warning"); // selects all the elements with class name of "warning"
+$("h1"); // selects all the h1s
+$("#heading"); // selects the element with id of "heading"
+$(".warning"); // selects all the elements with class name of "warning"
 
 // All the headers of h1 will be changed by these texts
 $("h1").text("jQuery is the greatest!");
